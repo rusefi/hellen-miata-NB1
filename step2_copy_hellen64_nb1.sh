2d646ec668b68f1a1dbc22aee5b0379475ea4113
andreika-git <andrei.home@gmail.com>
2022-03-17 23:01:44 +0200

    rev.C https://github.com/rusefi/hellen-NB1-issues/issues/32 https://github.com/rusefi/hellen-NB1-issues/issues/30 https://github.com/rusefi/hellen-NB1-issues/issues/28 https://github.com/rusefi/hellen-NB1-issues/issues/27

--- step2_copy_hellen64_nb1.sh
+++ step2_copy_hellen64_nb1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames" "../.." "../../gerber" "64_NB1" "b"
+python2.7 ./bin/copy_from_Kicad.py "frames:hellen" ".." "../../gerber" "64_NB1" "c"
 
 echo "Done!"
--- step3_create_hellen64_nb1.sh
+++ step3_create_hellen64_nb1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board.sh ".." "64_NB1" "b" "bom_replace_64_NB1-b.csv" "580,330"
+sh bin/create_board.sh ".." "64_NB1" "c" "bom_replace_64_NB1-c.csv" "580,330"
